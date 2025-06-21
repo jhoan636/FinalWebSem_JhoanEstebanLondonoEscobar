92aa9b1cddf88dd124bd0f337bdd9af89656cd70
jhoan636 <jhoan.63@hotmail.com>
2025-06-21 09:08:42 -0500

    Se genera una BD por defecto en caso que no creen el .env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI)
+const uri = process.env.MONGO_URI || 'mongodb+srv://jhoan636:butZfM9aU79Wuz2A@cluster0.tb5amb7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(uri)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error conectando a MongoDB', err));
 
